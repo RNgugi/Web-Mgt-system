ffabab71664ff958f16ecda83b428377b33c0296
RNgugi <reubeenkihiu@gmail.com>
2019-03-10 20:14:12 +0300

    more commits

--- signup/login.php
+++ signup/login.php
@@ -94,13 +94,14 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title>WritersPad &mdash; Sign Up</title>
+        <title>Warden &mdash; Login</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="apple-touch-icon" href="apple-touch-icon.png">
 
         <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
         <!--        <link rel="stylesheet" href="assets/css/bootstrap-theme.min.css">-->
+        <link rel="icon" href="../img/core-img/favicon.png">
 
 
         <!--For Plugins external css-->
